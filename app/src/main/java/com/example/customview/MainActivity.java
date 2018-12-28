ab4c992975933af05c179b8dde0ca1edf251beeb
王勇勇 <celb1121@163.com>
2018-12-28 17:59:00 +0800

    修改2

--- app/src/main/java/com/example/customview/MainActivity.java
+++ app/src/main/java/com/example/customview/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public int a = 1;
+    public int b = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
